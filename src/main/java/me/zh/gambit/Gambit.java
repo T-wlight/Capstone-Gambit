ffd1683367d1e4d6a5726ea430d848d81783ae75
zh.L <1913185725@qq.com>
2020-10-12 18:24:04 -0700

    first commit

---
+++ src/main/java/me/zh/gambit/Gambit.java
@@ -0,0 +1,21 @@
+package me.zh.gambit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author ziheng
+ * @since 2020.10.12
+ */
+public final class Gambit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
